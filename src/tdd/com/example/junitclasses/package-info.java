6f2f3ebf25b70e880901e4f66f0b7e523725e65a
Krishna Bahadur Bhat <krishna1bhat@Krishnas-MacBook-Pro.local>
2017-04-28 10:27:16 -0400

    JUnit feature - JUnit Classes information

---
+++ src/tdd/com/example/junitclasses/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author krishna1bhat
+ *
+ */
+package tdd.com.example.junitclasses;
+
+// 4. JUnit feature - JUnit Classes
+
+/*
+ * JUnit classes are used in writing and testing JUnits. 
+ * Some classes are:
+ * 1. Assert − Contains a set of assert methods.
+ * 2. TestCase − Contains a test case that defines the fixture to run multiple tests.
+ * 3. TestResult − Contains methods to collect the results of executing a test case. 
+ */
